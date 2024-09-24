04ad6050c93e33e7f4753627dd423c34702e53fe
FelipeTylinski <felipetylinski3@gmail.com>
2024-09-24 20:58:27 -0300

    aprendendo novo site

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+//$nome = "Felipe";
+//$idade = 27;
+//$altura = 1.82;
+
+
+//echo $idade + $altura;
+//echo $idade . $nome;
+
+//$usuario = array(
+   // "Felipe", 27, 1.82
+//); 
+
+// echo $usuario[2];
+
+//for ($i=4; $i <= 10; $i++) { 
+ //   echo $i."<br/>";
+//}
+
+ $nome = $_POST['nome'];
+ $texto = $_POST['texto'];
+ $sexo = $_POST['sexo'];
+
+ echo $nome."<br/>";
+ echo $texto."<br/>";
+ echo $sexo."<br/>";
+
+
+
+
+?>
