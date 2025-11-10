36b3fb87fe7061297c6146ecf5361a27fe5fa69a
FelipeTylinski <felipetylinski3@gmail.com>
2025-11-10 20:21:22 -0300

    Primeiro commit

---
+++ comuns/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-sclae=1">
+    <title>ALL In - Treinamentos</title>
+    <base href="http://localhost/">
+    <link rel="stylesheet" type="text/css" href="../recursos/css/main.css">
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="conteudo">
+                <div class="logo">
+                    <img src="recursos/imagens/logo.png">
+                    </div>
+                    <div class="menu-lateral">
+                    <ul class="nav">
+                <li>
+                    <a href="#">Quem sou eu</a>
+                </li>       
+                <li>
+                    <a href="#">Eventos</a>
+                </li>      <li>
+                    <a href="#">Cursos</a>
+                </li>      <li>
+                    <a href="#">Vídeos</a>
+                </li>      
+                <li>
+                    <a href="#">Fale comigo</a>
+                </li>      
+                    </ul>
+                            <div class="redes sociais">
+                                <p>Siga-me:</p>
+                                <a href="">Facebook</a>
+                                <a href="">Instagram</a>
+                                <a href="">Youtube</a>
+                        </div>
+                 </div>
+            </div>
+    </div>
+</div>
+</header>
+</body>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+    require_once('comuns/header.php');
+    require_once('views/home.php');
+    require_once('comuns/footer.php');
+
+?>
