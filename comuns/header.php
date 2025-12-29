ef7176a54afce02eaf142f5776e1455830045b55
FelipeTylinski <felipetylinski3@gmail.com>
2025-12-29 18:04:04 -0300

    hello world

--- comuns/header.php
+++ comuns/header.php
@@ -1,44 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-sclae=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>ALL In - Treinamentos</title>
-    <base href="http://localhost/">
-    <link rel="stylesheet" type="text/css" href="../recursos/css/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+    <link rel="stylesheet" type="text/css" href="recursos/css/main.css">
 </head>
+
 <body>
-    <header>
-        <div class="container">
-            <div class="conteudo">
-                <div class="logo">
-                    <img src="recursos/imagens/logo.png">
-                    </div>
-                    <div class="menu-lateral">
-                    <ul class="nav">
-                <li>
-                    <a href="#">Quem sou eu</a>
-                </li>       
-                <li>
-                    <a href="#">Eventos</a>
-                </li>      <li>
-                    <a href="#">Cursos</a>
-                </li>      <li>
-                    <a href="#">Vídeos</a>
-                </li>      
-                <li>
-                    <a href="#">Fale comigo</a>
-                </li>      
-                    </ul>
-                            <div class="redes sociais">
-                                <p>Siga-me:</p>
-                                <a href="">Facebook</a>
-                                <a href="">Instagram</a>
-                                <a href="">Youtube</a>
-                        </div>
-                 </div>
+<header>
+    <div class="container">
+        <div class="conteudo">
+            <div class="logo">
+                <img src="recursos/imagens/logo.png" alt="">
             </div>
+
+            <div class="menu-lateral">
+                <ul class="nav">
+                    <li><a href="#">Quem sou eu</a></li>
+                    <li><a href="#">Eventos</a></li>
+                    <li><a href="#">Cursos</a></li>
+                    <li><a href="#">Vídeos</a></li>
+                    <li><a href="#">Fale comigo</a></li>
+                </ul>
+
+                <div class="redes-sociais">
+                    <p>Siga-me:</p>
+                    <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    <a href="#"><i class="fa-brands fa-youtube"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-bar">
+            <i class="fa-solid fa-bars-staggered"></i>
+        </div>
     </div>
-</div>
 </header>
 </body>
+</html>
